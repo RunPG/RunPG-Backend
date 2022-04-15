2949db74ea66e817d0b86f4f772e05a3ce2a1739
Kieran Fooks <kieranfooks@gmail.com>
2022-04-15 21:18:16 +0200

    build: Add seed database

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      {
+        name: 'Gabriel'
+      },
+      {
+        name: 'Eliott'
+      },
+      {
+        name: 'Hugo'
+      },
+      {
+        name: 'Kieran'
+      }
+    ],
+    skipDuplicates: true
+  })
+}
+
+main()
+  .then(() => {
+    console.log('Migration done')
+  })
+  .catch(err => {
+    console.error(err)
+  })
