4d132683afdd9eb9a21706369f52b614f5f895b2
Kieran Fooks <kieranfooks@gmail.com>
2022-05-15 14:47:37 +0200

    feat: Add middleware logger

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express from 'express'
+import morgan from 'morgan'
 import { inventoryController, spellController, userController } from './controller'
 
 const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(morgan('common'))
 
 app.use('/user', userController)
 app.use('/inventory', inventoryController)
 app.use('/spell', spellController)
-
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`)
