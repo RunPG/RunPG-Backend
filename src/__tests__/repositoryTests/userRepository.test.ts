4c67986e427f682e118923d8957b9b91b666ebb6
KieranFooks <kieranfooks@gmail.com>
2022-05-31 00:36:03 +0200

    test: Setup Jest

---
+++ src/__tests__/repositoryTests/userRepository.test.ts
@@ -0,0 +1,19 @@
+import { prismaMock } from '../../prismaMock'
+import { userRepository } from '../../repository'
+import { User } from '@prisma/client'
+
+test('should get user with id 1', async () => {
+  const user: User = {
+    id: 1,
+    name: 'Gabriel',
+    lastCaloriesUpdate: new Date(),
+    guildId: null,
+    characterId: null
+  }
+
+  prismaMock.user.findUnique.mockResolvedValue(user)
+
+  const result = await userRepository.getById(1)
+
+  expect(result).toBe(user)
+})
