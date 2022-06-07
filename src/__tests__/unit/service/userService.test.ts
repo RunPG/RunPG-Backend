c698277a964d641a8b426d8c1e349b54ba36ff4a
KieranFooks <kieranfooks@gmail.com>
2022-06-07 18:37:21 +0200

    test: Create user service unit test

---
+++ src/__tests__/unit/service/userService.test.ts
@@ -0,0 +1,27 @@
+// jest.mock('../../../repository/userRepository.ts')
+import { userRepository } from '../../../repository'
+import { userService } from '../../../service'
+import { User } from '@prisma/client'
+
+/**
+ * User service unit test
+ *
+ * @group unit/service
+ */
+test('Get a user by name', async () => {
+  const expected : User = {
+    id: 1,
+    name: 'Test',
+    characterId: null,
+    guildId: null,
+    lastCaloriesUpdate: new Date()
+  }
+
+  userRepository.getByName = jest.fn(async () => {
+    return expected
+  })
+
+  const user = await userService.getByName('Test')
+
+  expect(user).toBe(expected)
+})
