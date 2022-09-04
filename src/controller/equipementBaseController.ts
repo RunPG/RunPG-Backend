d868ddb93fe3a293265ee7572d032d5de3269d5f
KieranFooks <kieranfooks@gmail.com>
2022-09-04 20:37:58 +0200

    doc(swagger): Update descriptions

--- src/controller/equipementBaseController.ts
+++ src/controller/equipementBaseController.ts
@@ -4,11 +4,20 @@ import { equipementBaseService } from '../service'
 
 const equipementBaseController = Router()
 
-
 equipementBaseController.get('/', async (req, res) => {
   /**
-  * #swagger.summary = 'Get equipement base'
-  */
+   * #swagger.summary = 'Get equipement base'
+   * #swagger.parameters['id'] = {
+       in: 'query',
+       required: false,
+       type: 'number',
+       description: 'Optional id of equipement base to search for',
+     }
+   * #swagger.responses[200] = { description: 'Equipement base found' }
+   * #swagger.responses[500] = { description: 'Server encountered an internal error' }
+   * #swagger.responses[400] = { description: 'Id is not valid' }
+   * #swagger.responses[404] = { description: 'Equipement base not found' }
+   */
   const id = Number(req.query.id)
   if (req.query.id != null && isNaN(id)) {
     res.sendStatus(StatusCodes.BAD_REQUEST)
--- src/controller/itemController.ts
+++ src/controller/itemController.ts
@@ -6,8 +6,12 @@ const itemController = Router()
 
 itemController.get('/', async (req, res) => {
   /**
-  * #swagger.summary = 'Get items'
-  */
+   * #swagger.summary = 'Get items'
+   * #swagger.responses[200] = { description: 'User inventory updated' }
+   * #swagger.responses[500] = { description: 'Server encountered an internal error' }
+   * #swagger.responses[400] = { description: 'id or quantity is not valid' }
+   * #swagger.responses[404] = { description: 'Inventory does not exist' }
+   */
   const id = Number(req.query.id)
   if (req.query.id != null && isNaN(id)) {
     res.sendStatus(StatusCodes.BAD_REQUEST)
